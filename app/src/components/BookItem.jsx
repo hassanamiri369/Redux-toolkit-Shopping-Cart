06650f7ba03c3a5feea1110bf93b292a15ce04d9
Hassanamiri <hassanamiri369@gmail.com>
2023-04-15 13:51:08 +0330

    fix product item style

--- app/src/components/BookItem.jsx
+++ app/src/components/BookItem.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const BookItem = ({key , book}) => {
   return (
-    <div className='bookItem-container'>
-        <div>{book.name}</div>
+    <div className='bookItem-container' style={{width : "250px",margin :"10px",border : "1px solid black"}}>
+        <img width={250} height={250} src={book.imageUrl} alt='product name'/>
+        <div className='content'>
+        <p className='name'>{book.name}</p>
+        <p className='author'>{book.author}</p>
+        <p className='box'>
+            <span className='price'>{book.price}$</span>
+            <span className='addToCart'><button>Add to cart</button></span>
+        </p>
+        </div>
     </div>
   )
 }
