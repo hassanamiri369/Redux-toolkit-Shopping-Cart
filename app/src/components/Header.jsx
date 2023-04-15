d67421944542aa34d8063592744ee8b6fd112208
Hassanamiri <hassanamiri369@gmail.com>
2023-04-15 13:05:24 +0330

    adding home and cart link to header component

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import {Link} from "react-router-dom";
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header'>
+        <div className='home-link'>
+            <Link to={'/'}>Shopping</Link>
+        </div>
+        <div className='cart-link'>
+            <Link to={'/cart'}>Cart</Link>
+        </div>
+    </div>
   )
 }
 
