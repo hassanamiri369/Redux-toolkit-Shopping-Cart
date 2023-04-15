cc6ce3783d564531f7e343b0dacf7648325bfc36
Hassanamiri <hassanamiri369@gmail.com>
2023-04-15 12:57:55 +0330

    adding BrowserRouter in Root component

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// config router
+import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
   </React.StrictMode>,
 )
