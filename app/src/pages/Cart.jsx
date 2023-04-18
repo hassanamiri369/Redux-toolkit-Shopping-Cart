6ec71017a7cb7124fb938dff945a6614b37de30d
Hassanamiri <hassanamiri369@gmail.com>
2023-04-18 15:04:39 +0330

    show product in cart

--- app/src/pages/Cart.jsx
+++ app/src/pages/Cart.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import CartItem from '../components/CartItem';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Cart = () => {
+
+  const dispatch = useDispatch;
+  const cartitem = useSelector(state => state.cart)
+
+
+
   return (
-    <div className='cart-container'>Cart</div>
+    <>
+      <div className='cart-count'>Count Product in Cart : {cartitem.cart?.length}</div>
+
+      {cartitem.cart?.length === 0 ? <h2 style={{paddingLeft : "40px" , color : "red"}}>Cart is Empty</h2> :
+        <CartItem/>
+
+      }
+    </>
   )
 }
 
