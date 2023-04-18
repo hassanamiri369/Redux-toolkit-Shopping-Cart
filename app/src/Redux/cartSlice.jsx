de844533f05adb0341cf1cd14ccf90073aceb369
Hassanamiri <hassanamiri369@gmail.com>
2023-04-18 12:53:22 +0330

    change addTocart action functionality

--- app/src/Redux/cartSlice.jsx
+++ app/src/Redux/cartSlice.jsx
@@ -2,15 +2,24 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
     name : "cart",
-    initialState : {list : [] , total : 0},
+    initialState : {cart : []},
     reducers: {
+        // AddToCart (state , action){
+        //     const check = state.list.findIndex(book => book.id ===action.payload.id)
+        //     if(check !== -1){
+        //         state.list[index].quantity += action.payload.quantity
+        //     }else{
+        //         state.list.push(action.payload)
+        //     }
+        // }
+
         AddToCart (state , action){
-            const check = state.list.findIndex(book => book.id ===action.payload.id)
-            if(check !== -1){
-                state.list[index].quantity += action.payload.quantity
-            }else{
-                state.list.push(action.payload)
-            }
+            const newItem = action.payload;
+            const existItem = state.cart.find(item => item.id === newItem.id)
+            const cartItems = existItem ? state.cart.map(item => item.id === existItem.id ? newItem : item)
+            :
+            [...state.cart , newItem]
+            return {...state , cart : cartItems}
         }
     }
 })
