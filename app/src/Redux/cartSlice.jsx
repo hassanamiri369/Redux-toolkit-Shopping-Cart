27073512ec292a49dd996b5247bd648899d2ca6b
Hassanamiri <hassanamiri369@gmail.com>
2023-04-23 17:25:30 +0330

    handle resetCart and deleteProduct action

--- app/src/Redux/cartSlice.jsx
+++ app/src/Redux/cartSlice.jsx
@@ -4,22 +4,37 @@ const cartSlice = createSlice({
     name : "cart",
     initialState : {cart : []},
     reducers: {
-        // AddToCart (state , action){
-        //     const check = state.list.findIndex(book => book.id ===action.payload.id)
-        //     if(check !== -1){
-        //         state.list[index].quantity += action.payload.quantity
-        //     }else{
-        //         state.list.push(action.payload)
-        //     }
-        // }
+       
 
         AddToCart (state , action){
             const newItem = action.payload;
+            
             const existItem = state.cart.find(item => item.id === newItem.id)
-            const cartItems = existItem ? state.cart.map(item => item.id === existItem.id ? newItem : item)
+            const quantity = existItem ? existItem.quantity + 1 : 1;
+            const updatedProduct = {...newItem , quantity}
+            const cartItems = existItem ? state.cart.map(item => item.id === existItem.id ? updatedProduct : item)
             :
-            [...state.cart , newItem]
+            [...state.cart , updatedProduct]
             return {...state , cart : cartItems}
+        } ,
+
+        deleteProduct (state ,action){
+            const newItem = action.payload;
+            const exist =  state.cart.find(x => x.id === newItem.id)
+            if(exist.quantity === 1){
+                const updated = state.cart.filter(x => x.id !== newItem.id)
+                return {...state , cart : updated}
+            }else{
+                const updated = state.cart.map(x => x.id === newItem.id ? {...x , quantity : x.quantity - 1} : x)
+                return {...state , cart : updated}
+            }
+        }, 
+
+
+        resetCart (state , action){
+            const newItem = action.payload;
+            const updated = state.cart.filter(x => x.id !== newItem.id)
+                return {...state , cart : updated}
         }
     }
 })
@@ -28,7 +43,7 @@ const cartSlice = createSlice({
 
 
 
-export const {AddToCart} = cartSlice.actions;
+export const {AddToCart , deleteProduct , resetCart} = cartSlice.actions;
 
 export default cartSlice.reducer;
 
